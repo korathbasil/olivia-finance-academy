5686b1d5d82e720d6a69dba88ebb5efbc1c6f735
korathbasil <korathbasil@live.com>
2020-11-13 22:30:32 +0530

    working on contact pagr

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const loader = document.getElementById("loader");
+const nav = document.getElementById("responsive-nav");
+const navButton = document.getElementById("responsive-nav-button");
+let showResponsiveNav = false;
 
 window.onload = () => {
   setTimeout(() => {
@@ -8,4 +11,17 @@ window.onload = () => {
 
 const turnOffLoader = () => {
   loader.style.display = "none";
+};
+
+// navButton.addEventListener("click", responsiveNavHandler);
+
+const responsiveNavHandler = () => {
+  console.log("Clicked");
+  if (showResponsiveNav) {
+    nav.style.display = "none";
+    showResponsiveNav = false;
+  } else {
+    nav.style.display = "inline";
+    showResponsiveNav = true;
+  }
 };
