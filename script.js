f99f7ba4c9a38fbc11965b5a55dd331f0fd8486b
korathbasil <korathbasil@live.com>
2020-11-20 01:22:22 +0530

    changed loader animation behaviour

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.getElementById("body");
 const loader = document.getElementById("loader");
 const nav = document.getElementById("responsive-nav");
 const navButton = document.getElementById("responsive-nav-button");
@@ -5,15 +6,17 @@ let showResponsiveNav = false;
 
 window.onload = () => {
   setTimeout(() => {
-    turnOffLoader();
+    body.style.overflowY = "scroll";
+    loader.style.opacity = 0;
+    setTimeout(() => {
+      turnOffLoader();
+    }, 400);
   }, 2000);
 };
 
 const turnOffLoader = () => {
   loader.style.display = "none";
 };
-
-// navButton.addEventListener("click", responsiveNavHandler);
 
 const responsiveNavHandler = () => {
   console.log("Clicked");
